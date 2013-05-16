323a602f3ff736770aaf38ae0c84b32c3b92ed26
vassili <vassili@academyworkstation01centralwaycom.local>
2013-05-16 18:38:12 +0200

    add tests

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,6 +2,12 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
-
+describe('PhoneCat controllers', function() {
+	describe('faceListCtrl', function(){
+		it('should create "faces" model with 2 faces', function(){
+			var scope = {},
+				ctrl = new faceListCtrl(scope);
+			expect(scope.faces.length).toBe(2);
+		});
+	});
 });
