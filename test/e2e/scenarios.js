595e054e7f127f9faf5f572c88db464b47947a4b
vassili <vassili@academyworkstation01centralwaycom.local>
2013-05-17 11:00:17 +0200

    add search field

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -2,7 +2,16 @@
 
 /* http://docs.angularjs.org/guide/dev_guide.e2e-testing */
 
-describe('my app', function() {
+describe('Faces App', function() {
+	describe('Faces list view', function() {
+		beforeEach(function(){
+			browser().navigateTo('../../app/index.html');
+		});
+		it('Should filter the face list as user types into the search box',function(){
+			expect(repeater('.faces').count()).toBe(3);
 
-
+			input('ddd').enter('che');
+			expect(repeater('.faces').count()).toBe(1);
+		});
+	});
 });
