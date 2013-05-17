32e974a5bcd30a14a9d9fd33ef3236ad3964af50
vassili <vassili@academyworkstation01centralwaycom.local>
2013-05-17 11:24:45 +0200

    add more tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -13,5 +13,14 @@ describe('Faces App', function() {
 			input('ddd').enter('che');
 			expect(repeater('.faces').count()).toBe(1);
 		});
+		it('should display the current filter value within an element with id "status"',
+			function(){
+				expect(element('#status').text()).toMatch(/Current filter: \s*$/);
+
+				input('ddd').enter('che');
+
+				expect(element('#status').text()).toMatch(/Current filter: che\s*$/);
+				//using('#status').expect(binding('ddd')).toBe('che');
+			})
 	});
 });
